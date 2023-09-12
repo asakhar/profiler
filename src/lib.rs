214d6b23c821eb4a35d2da55604e815311f78366
asakhar <a@sakhar.ru>
2023-09-12 21:16:06 +0300

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+use std::{fs::File, io::Write, sync::Mutex};
+
+use chrono::{DateTime, Local};
+use lazy_static::lazy_static;
+
+pub struct ProfileLogger {
+  file: Mutex<File>,
+}
+impl ProfileLogger {
+  pub fn entry(
+    &self,
+    inner_name: &str,
+    time: std::time::Duration,
+    start: std::time::SystemTime,
+    end: std::time::SystemTime,
+    module: &str,
+  ) {
+    let mut file = self.file.lock().unwrap();
+    let start = DateTime::<Local>::from(start);
+    let end = DateTime::<Local>::from(end);
+    _ = writeln!(
+      file,
+      "{module}::{inner_name} => {} {} {}",
+      time.as_micros(),
+      start,
+      end
+    );
+    _ = file.flush();
+  }
+}
+
+lazy_static! {
+  pub static ref GLOBAL_PROFILER: ProfileLogger = ProfileLogger {
+    file: Mutex::new(File::create("profiler.log").unwrap())
+  };
+}
